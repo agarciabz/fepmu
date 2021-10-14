a37f9a4f0c2e38d1ee08c2f94c96e098a0cd34fd
agarciabz <agusgb93@gmail.com>
2021-10-14 22:33:10 +0200

    Update prefix

--- apps/fepmu/src/app/app.component.ts
+++ apps/fepmu/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'fepmu-root',
+  selector: 'pmu-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
