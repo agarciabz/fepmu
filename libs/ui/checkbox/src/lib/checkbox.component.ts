a47e37e539277a056e28310c82c639975b618c83
Agustin Garcia <a.garcia@pnptc.com>
2021-10-16 12:12:11 +0200

    Add control value accessor to checkbox

--- libs/ui/checkbox/src/lib/checkbox.component.ts
+++ libs/ui/checkbox/src/lib/checkbox.component.ts
@@ -1,17 +1,58 @@
-import { Component, OnInit, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewEncapsulation,
+  ChangeDetectionStrategy,
+  HostListener,
+  forwardRef,
+  ChangeDetectorRef
+} from '@angular/core';
+import { ControlValueAccessor } from '@ngneat/reactive-forms';
+import { NG_VALUE_ACCESSOR } from '@angular/forms';
+
+export const CHECKBOX_VALUE_ACCESSOR = {
+  provide: NG_VALUE_ACCESSOR,
+  useExisting: forwardRef(() => CheckboxComponent),
+  multi: true
+};
 
 @Component({
   selector: ' pmu-checkbox',
   templateUrl: './checkbox.component.html',
   styleUrls: ['./checkbox.component.scss'],
   encapsulation: ViewEncapsulation.Emulated,
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    '(change)': 'onChange($event.target.checked)',
+    '(blur)': 'onTouched()'
+  },
+  providers: [
+    CHECKBOX_VALUE_ACCESSOR
+  ]
 })
-export class CheckboxComponent implements OnInit {
+export class CheckboxComponent extends ControlValueAccessor<boolean> {
 
-  constructor() { }
+  public value = false;
+  public disabled = false;
 
-  ngOnInit(): void {
+  @HostListener('change', ['$event.target.checked'])
+  onChange = (checked: boolean | null) => {};
+
+  @HostListener('blur')
+  onTouched: () => void = () => {};
+
+  constructor(private cdr: ChangeDetectorRef) {
+    super();
   }
+
+  writeValue(value: boolean) {
+    this.value = value;
+    this.cdr.detectChanges();
+  }
+
+  setDisabledState(isDisabled: boolean) {
+    this.disabled = isDisabled;
+  }
+
 
 }
