15c61e7b1431aeb6f10167c9fcdb24c6b1c21efe
agarciabz <agusgb93@gmail.com>
2022-01-13 19:39:50 +0100

    Add github pipeline

--- apps/fepmu-app/src/app/app.module.ts
+++ apps/fepmu-app/src/app/app.module.ts
@@ -7,7 +7,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { ThreeHousesModule } from "@fepmu/features/three-houses";
-import {TuiSelectModule} from "@taiga-ui/kit";
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
