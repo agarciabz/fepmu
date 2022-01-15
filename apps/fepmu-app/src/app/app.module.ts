2399f287d320af2b4d1707899973c46e06497979
agarciabz <agusgb93@gmail.com>
2022-01-15 19:00:35 +0100

    Revert "Revert commit"
    This reverts commit 5bd78cec21e21dc876faf3b57346753652d3cd1f.

--- apps/fepmu-app/src/app/app.module.ts
+++ apps/fepmu-app/src/app/app.module.ts
@@ -1,31 +1,17 @@
-import { NgDompurifySanitizer } from '@tinkoff/ng-dompurify';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {
-  TuiRootModule,
-  TuiDialogModule,
-  TuiNotificationsModule,
-  TUI_SANITIZER,
-  TuiModeModule,
-} from '@taiga-ui/core';
+import { NgDompurifySanitizer } from "@tinkoff/ng-dompurify";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { TuiRootModule, TuiDialogModule, TuiNotificationsModule, TUI_SANITIZER } from "@taiga-ui/core";
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ThreeHousesModule } from '@fepmu/features/three-houses';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { ThreeHousesModule } from "@fepmu/features/three-houses";
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [
-    BrowserModule,
-    TuiRootModule,
-    BrowserAnimationsModule,
-    TuiDialogModule,
-    TuiNotificationsModule,
-    ThreeHousesModule,
-    TuiModeModule,
-  ],
-  providers: [{ provide: TUI_SANITIZER, useClass: NgDompurifySanitizer }],
+  imports: [BrowserModule, TuiRootModule, BrowserAnimationsModule, TuiDialogModule, TuiNotificationsModule, ThreeHousesModule],
+  providers: [{provide: TUI_SANITIZER, useClass: NgDompurifySanitizer}],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- libs/features/three-houses/src/lib/three-houses.module.ts
+++ libs/features/three-houses/src/lib/three-houses.module.ts
@@ -1,38 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ThreeHousesFormComponent } from './components/three-houses-form/three-houses-form.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {
   TuiCheckboxLabeledModule,
   TuiDataListWrapperModule,
   TuiInputCountModule,
-  TuiSelectModule,
-} from '@taiga-ui/kit';
-import {
-  TuiButtonModule,
-  TuiDataListModule,
-  TuiModeModule,
-  TuiTextfieldControllerModule,
-} from '@taiga-ui/core';
+  TuiSelectModule
+} from "@taiga-ui/kit";
+import {TuiButtonModule, TuiDataListModule, TuiTextfieldControllerModule} from "@taiga-ui/core";
 import { ThreeHousesContainerComponent } from './containers/three-houses-container/three-houses-container.component';
-import { UnitModule } from '@fepmu/ui/unit';
+import {UnitModule} from "@fepmu/ui/unit";
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    ReactiveFormsModule,
-    TuiSelectModule,
-    TuiDataListModule,
-    TuiDataListWrapperModule,
-    TuiCheckboxLabeledModule,
-    TuiInputCountModule,
-    TuiTextfieldControllerModule,
-    TuiButtonModule,
-    TuiModeModule,
-    UnitModule,
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, TuiSelectModule, TuiDataListModule, TuiDataListWrapperModule,
+    TuiCheckboxLabeledModule, TuiInputCountModule, TuiTextfieldControllerModule, TuiButtonModule, UnitModule],
+  declarations: [
+    ThreeHousesFormComponent,
+    ThreeHousesContainerComponent
   ],
-  declarations: [ThreeHousesFormComponent, ThreeHousesContainerComponent],
-  exports: [ThreeHousesFormComponent, ThreeHousesContainerComponent],
+  exports: [
+    ThreeHousesFormComponent,
+    ThreeHousesContainerComponent,
+  ],
 })
 export class ThreeHousesModule {}
