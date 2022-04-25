40b97dcfbc63eee066dc386bb88f586640c66442
Agustin Garcia <a.garcia@pnptc.com>
2022-04-25 14:20:56 +0200

    wip form

--- libs/ui/react/features/three-houses/src/components/three-houses-form/three-houses-form.tsx
+++ libs/ui/react/features/three-houses/src/components/three-houses-form/three-houses-form.tsx
@@ -7,7 +7,11 @@ import {
   NumberIncrementStepper,
   NumberDecrementStepper,
   Select,
+  FormControl,
+  FormLabel,
 } from '@chakra-ui/react';
+import { Options, defaultOptions } from '@fepmu/data/three-houses';
+import { ChangeEvent, useEffect, useState } from 'react';
 
 /* eslint-disable-next-line */
 export interface ThreeHousesFormProps {}
@@ -20,43 +24,162 @@ export function ThreeHousesForm(props: ThreeHousesFormProps) {
     'Silver Snow',
   ];
   const avatarGender = ['male', 'female', 'random'];
+
+  const [state, setState] = useState<Options>(defaultOptions);
+  useEffect(() => {
+    if (!state.randomizeClasses) {
+      setState({
+        ...state,
+        allowInviableBuilds: false,
+        balanceRoster: false,
+      });
+    }
+  }, [state.randomizeClasses]);
+
+  const isCheckbox = (
+    x: HTMLSelectElement | HTMLInputElement
+  ): x is HTMLInputElement => x.type === 'checkbox';
+
+  const handleInputChange = (
+    event: ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    const target = event.target;
+    const value = isCheckbox(target) ? target.checked : target.value;
+    const name = target.name;
+
+    setState({
+      ...state,
+      [name]: value,
+    });
+  };
+
+  const handleRosterSizeChange = (_: string, size: number) =>
+    setState({ ...state, rosterSize: size });
+
+  const formSubmit = (event: any) => console.log(state);
+
   return (
-    <div className="space-y-4">
-      <div className="space-y-2 flex flex-col">
-        <Select>
-          {routes.map((item) => (
-            <option key={item} value={item}>
-              {item}
-            </option>
-          ))}
-        </Select>
-        <Select>
-          {avatarGender.map((item) => (
-            <option key={item} value={item}>
-              {item}
-            </option>
-          ))}
-        </Select>
-        <Checkbox>Randomize classes</Checkbox>
-        <Checkbox>Allow inviable builds</Checkbox>
-        <Checkbox>Allow other houses</Checkbox>
-        <Checkbox>Balance roster</Checkbox>
-        <Checkbox>Include free updates</Checkbox>
-        <Checkbox>Include season pass</Checkbox>
-        <NumberInput defaultValue={15} min={10} max={20}>
-          <NumberInputField />
-          <NumberInputStepper>
-            <NumberIncrementStepper />
-            <NumberDecrementStepper />
-          </NumberInputStepper>
-        </NumberInput>
+    <form>
+      <div className="space-y-4">
+        <div className="space-y-2 flex flex-col">
+          <FormControl>
+            <FormLabel htmlFor="route">Route</FormLabel>
+            <Select
+              value={state.route}
+              id="route"
+              name="route"
+              onChange={handleInputChange}
+            >
+              {routes.map((item) => (
+                <option key={item} value={item}>
+                  {item}
+                </option>
+              ))}
+            </Select>
+          </FormControl>
+          <FormControl>
+            <FormLabel htmlFor="avatarGender">Avatar gender</FormLabel>
+            <Select
+              value={state.avatarGender}
+              id="avatarGender"
+              name="avatarGender"
+              onChange={handleInputChange}
+            >
+              {avatarGender.map((item) => (
+                <option key={item} value={item}>
+                  {item}
+                </option>
+              ))}
+            </Select>
+          </FormControl>
+          <FormControl>
+            <Checkbox
+              id="randomizeClasses"
+              name="randomizeClasses"
+              checked={state.randomizeClasses}
+              onChange={handleInputChange}
+            >
+              Randomize classes
+            </Checkbox>
+          </FormControl>
+          <FormControl isDisabled={!state.randomizeClasses}>
+            <Checkbox
+              id="allowInviableBuilds"
+              name="allowInviableBuilds"
+              checked={state.allowInviableBuilds}
+              onChange={handleInputChange}
+            >
+              Allow inviable builds
+            </Checkbox>
+          </FormControl>
+          <FormControl>
+            <Checkbox
+              id="allowOtherHouses"
+              name="allowOtherHouses"
+              checked={state.allowOtherHouses}
+              onChange={handleInputChange}
+            >
+              Allow other houses
+            </Checkbox>
+          </FormControl>
+          <FormControl isDisabled={!state.randomizeClasses}>
+            <Checkbox
+              id="balanceRoster"
+              name="balanceRoster"
+              checked={state.balanceRoster}
+              onChange={handleInputChange}
+            >
+              Balance roster
+            </Checkbox>
+          </FormControl>
+          <FormControl>
+            <Checkbox
+              id="includeFreeUpdates"
+              name="includeFreeUpdates"
+              checked={state.includeFreeUpdates}
+              onChange={handleInputChange}
+            >
+              Include free updates
+            </Checkbox>
+          </FormControl>
+          <FormControl>
+            <Checkbox
+              id="includeSeasonPass"
+              name="includeSeasonPass"
+              checked={state.includeSeasonPass}
+              onChange={handleInputChange}
+            >
+              Include season pass
+            </Checkbox>
+          </FormControl>
+          <FormControl>
+            <FormLabel htmlFor="rosterSize">Roster size</FormLabel>
+            <NumberInput
+              value={state.rosterSize}
+              min={2}
+              max={12}
+              onChange={handleRosterSizeChange}
+            >
+              <NumberInputField id="rosterSize" name="rosterSize" />
+              <NumberInputStepper>
+                <NumberIncrementStepper />
+                <NumberDecrementStepper />
+              </NumberInputStepper>
+            </NumberInput>
+          </FormControl>
+        </div>
+        <div className="flex">
+          <Button
+            className="grow"
+            size={'lg'}
+            type="button"
+            onClick={formSubmit}
+          >
+            Pick my units
+          </Button>
+        </div>
       </div>
-      <div className="flex">
-        <Button className="grow" size={'lg'}>
-          Pick my units
-        </Button>
-      </div>
-    </div>
+    </form>
   );
 }
 
