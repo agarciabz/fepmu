b47f845e1618c4dc6cb0619f6ee4a3beef9cb854
Agustin Garcia <a.garcia@pnptc.com>
2022-04-30 17:17:13 +0200

    Update cicd pipeline

--- libs/ui/react/features/three-houses/src/containers/three-houses-container/three-houses-container.tsx
+++ libs/ui/react/features/three-houses/src/containers/three-houses-container/three-houses-container.tsx
@@ -1,34 +1,50 @@
-import { Pick, mockPicks, Options } from '@fepmu/data/three-houses';
+import {
+  Pick,
+  mockPicks,
+  Options,
+  applyFilters,
+  getUnitsText,
+} from '@fepmu/data/three-houses';
 import { UnitList } from '@fepmu/ui/react/unit-list';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import ThreeHousesForm from '../../components/three-houses-form/three-houses-form';
 import { Button } from '@chakra-ui/react';
+
+const createText = (picks: Pick[], route: string) =>
+  ['Fire Emblem Three Houses PMU', route, '', getUnitsText(picks)].join('\n');
 
 /* eslint-disable-next-line */
 export interface ThreeHousesContainerProps {}
 
-export interface State {
-  picks: Pick[];
-}
-
 export function ThreeHousesContainer(props: ThreeHousesContainerProps) {
-  const [state] = useState<State>({
-    picks: mockPicks,
-  });
-  const { picks } = state;
+  const [options, setOptions] = useState<Options | undefined>(undefined);
+  const [picks, setPicks] = useState<Pick[]>([]);
+  const [text, setText] = useState<string>('');
 
-  const handleSubmit = (options: Options) => console.log(options);
+  useEffect(() => {
+    if (options) setPicks(applyFilters(options));
+  }, [options]);
+
+  useEffect(() => {
+    if (picks.length && options) setText(createText(picks, options.route));
+  }, [picks]);
+
+  const handleSubmit = (options: Options) => setOptions(options);
+
+  const copyToClipboard = () => navigator.clipboard.writeText(text).then();
 
   return (
     <div className="p-4 flex flex-col space-y-4 md:w-3/5 lg:w-2/5 md:mx-auto">
       <ThreeHousesForm onSubmit={handleSubmit} />
-      <UnitList picks={picks}>Selected units</UnitList>
       {picks.length > 0 ? (
-        <div className="flex">
-          <Button className="grow" size={'lg'}>
-            Copy to clipboard
-          </Button>
-        </div>
+        <>
+          <UnitList picks={picks}>Selected units</UnitList>
+          <div className="flex">
+            <Button className="grow" size={'lg'} onClick={copyToClipboard}>
+              Copy to clipboard
+            </Button>
+          </div>
+        </>
       ) : (
         <></>
       )}
